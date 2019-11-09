720c5e07d3b8cb00d5934fb693684b27cad8bafd
dsjack <dsjack89@gmail.com>
2019-11-08 20:31:20 -0800

    Puts airtable module methods into a class

--- airtable_module.py
+++ airtable_module.py
@@ -1,32 +1,36 @@
 from airtable import Airtable
 
 
+class DataRouter:
+    def __init__(self, base, table_name, api_key, assignee):
+        self._base = base
+        self._table_name = table_name
+        self._api_key = api_key
+        self._assignee = assignee
 
-airtable = Airtable('appxihFX0qEmouKsg', 'Content production',
-                    api_key='keyHPaW1yiQPDcJFN').get_all()
+        self.airtable = Airtable(self._base, self._table_name,
+                                 api_key=self._api_key)
+        self.unfiltered_table = self.airtable.get_all()
+        self.filtered_table = self.filter_by_assigned()
 
+    def filter_by_assigned(self):
+        """
+        Filters Airtable.get_all() by assignee passed to Class
+        """
+        if self._assignee is None:
+            print('Cannot filter. No assignee passed to class.')
+            return None
+        filtered_table = list()
+        for row in self.airtable.get_all():
+            if row['fields']['Editor']['name'] == self._assignee:
+                filtered_table.append(row)
+        return filtered_table
 
-def filter_by_assigned(table, name):
-    """
-    :param table: Instance of Airtable.get_all()
-    :param name: Assigned name to filter by
-    :return: List of Airtable rows assigned to :param name
-    """
-    filtered_table = list()
-    for row in table:
-        if row['fields']['Author']['name'] == name:
-            filtered_table.append(row)
-    return filtered_table
+    def print_assigned(self, filtered=False):
+        table = self.unfiltered_table
+        if filtered is True:
+            table = self.filtered_table
 
-
-filtered_table = filter_by_assigned(airtable, 'Cameron Toth')
-for i in filtered_table:
-    print(i)
-    print("\n")
-
-
-
-# 1. Pull data from airtable calendar when: blog is assigned to Janey (editor
-# column),
-# 2. place data in an object
-# 3. Send and add object to Asana
+        for i in table:
+            print(i)
+            print("\n")
