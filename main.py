3e9ff56a334ce98f475775c423e377e5c225ba35
dsjack <dsjack89@gmail.com>
2019-11-10 09:44:46 -0800

    Adds text file to store latest match fetched createdTime, methods to get and set it

--- main.py
+++ main.py
@@ -4,24 +4,39 @@ from airtable_fetcher import TableFetcher
 import credentials
 
 
-airtable = Airtable(credentials.jane_at_base, credentials.jane_at_table,
+def main_loop(fetcher, taskmaster):
+    table_rows = fetcher.prep_matches()
+    if len(table_rows) == 0:
+        print('No new tasks to add to Asana. Asana is up to date.')
+    else:
+        for row in table_rows:
+            taskmaster.add_task(row['title'], 'Draft: {}\n\nIssue: {}'
+                                .format(row['draft'], row['issue']))
+
+
+def get_latest_datetime():
+    with open('created_at.txt', 'r') as date_time:
+        print('GETTING...')
+        return date_time.read()
+
+
+def set_latest_datetime(fetcher):
+    latest = fetcher.get_latest_match_time()
+    print('SETTING...')
+    with open('created_at.txt', 'w') as date_time:
+        date_time.write(latest)
+
+
+airtable = Airtable(credentials.jane_at_base,
+                    credentials.jane_at_table,
                     api_key=credentials.jane_at_api)
+
 
 table_fetcher = TableFetcher(airtable,
                              credentials.jane_at_name,
-                             '2019-11-01T19:07:39.000Z')
-table_fetcher.print_matches(asana_preview=True)
+                             get_latest_datetime())
 task_master = Taskmaster(credentials.dan_asana_pat, 'NSC')
 
+set_latest_datetime(table_fetcher)
 
-def main_loop(fetcher, taskmaster):
-    table_rows = fetcher.prep_matches()
-    for row in table_rows:
-        taskmaster.add_task(row['title'], 'Draft: {}\n\nIssue: {}'.format(
-            row['draft'], row['issue']))
-
-
-# print(table_fetcher.get_latest_match_time())
 main_loop(table_fetcher, task_master)
-
-
