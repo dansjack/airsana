a923ab9bfd031a60e8d05df825913840765ae0ef
dsjack <dsjack89@gmail.com>
2019-12-27 19:30:54 -0800

    Moves certain methods to a new utility package, updates docstrings in taskmaster module

--- asana_taskmaster.py
+++ asana_taskmaster.py
@@ -9,19 +9,16 @@ class Taskmaster:
         :param pat: Asana Personal Access Token
         :param workspace_name: The workspace name to add a task to
         """
-
         self._pat = pat
+        self._workspace_name = workspace_name
         self._client = asana.Client().access_token(self._pat)
         self._set_client_options()
-        self._workspace_name = workspace_name
         self._workspace_gid = self._get_workspace_gid()
         self._assignee = self._client.users.me()
 
     def _set_client_options(self):
         """
-        Sets client options, suppresses Asana deprecation warnings
-
-        :return void
+        :return void. Sets client options, suppresses Asana deprecation warnings
         """
         self._client.LOG_ASANA_CHANGE_WARNINGS = False
         self._client.options['pretty'] = True
@@ -36,7 +33,11 @@ class Taskmaster:
                 return workspace['gid']
         return -1
 
-    def find_all_incomplete(self):
+    def _find_all_incomplete(self):
+        """
+        For testing purposes only
+        :return: Prints all tasks marked 'incomplete' in asana
+        """
         for task in self._client.tasks.find_all(workspace=self._workspace_gid,
                                                 assignee=self._assignee['gid']):
             if not task['completed']:
@@ -44,16 +45,11 @@ class Taskmaster:
 
     def add_task(self, name, notes):
         """
-        Posts a task to the object's workspace
-
         :param name: Name of the task
         :param notes: Notes for the task
-        :return: void
+        :return: void. Posts a task to the object's workspace
         """
         self._client.tasks.create_in_workspace(self._workspace_gid,
                                                {'name': name,
                                                 'notes': notes,
                                                 'assignee': self._assignee})
-
-        print('Task named "{}" added to workspace: {}'
-              .format(name, self._workspace_name))
