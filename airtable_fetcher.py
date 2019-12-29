ce46370e8d6907279acec83e324cf722bebd6962
dsjack <dsjack89@gmail.com>
2019-12-28 19:29:11 -0800

    Adds a static method for adding fields to airtable_fetcher, replaces more hardcoded fields

--- airtable_fetcher.py
+++ airtable_fetcher.py
@@ -1,3 +1,34 @@
+def add_match_field(table_row, match_row, field, sub_field=None):
+    """
+    Catch any missing data in one row of an Airtable calendar
+    :param table_row: dict. an airtable calendar row
+    :param match_row: dict. an object to store match data
+    :param field: string.
+    :param sub_field: string (optional).
+    :return:
+    """
+    f_lower = field.lower()
+    if sub_field == 'name':
+        f_lower = 'assignee'
+    elif 'title' in field.lower() or 'headline' in field.lower():
+        f_lower = 'title'
+
+    if sub_field:
+        try:
+            match_row[f_lower] = table_row['fields'][field][sub_field]
+        except KeyError:  # No value at field/sub-field for this match
+            print('Blog with id #{} has no value at: ["{}"]["{}"]...'.format(
+                table_row['id'], field, sub_field))
+            match_row[f_lower] = ""
+    else:
+        try:
+            match_row[f_lower] = table_row['fields'][field]
+        except KeyError:  # No value at field for this match
+            print('Blog with id #{} has no value at: ["{}"]...'.format(
+                table_row['id'], field))
+            match_row[f_lower] = ""
+
+
 class TableFetcher:
     def __init__(self, airtable, assignee, last_fetched):
         """
@@ -15,7 +46,6 @@ class TableFetcher:
         """
         Filters Airtable.get_all() by assignee passed to Class
         """
-        # filtered_table = list()
         print('GETTING matches with createdTime later than {}'.format(
             self._last_fetched))
         for row in self._airtable.get_all():
@@ -30,10 +60,10 @@ class TableFetcher:
                     print('Blog with id #{} not assigned. Skipping...'.format(
                         row['id']))
 
-    def print_matches(self, asana_preview=False):
+    def print_matches(self, match_structure, asana_preview=False):
         table = self._filtered_table
         if asana_preview:
-            table = self.prep_matches()
+            table = self.prep_matches(match_structure)
         for i in table:
             print(i)
             print("\n")
@@ -44,27 +74,16 @@ class TableFetcher:
         matches = list()
         for row in self._filtered_table:
             match_row = dict()
-            match_row['title'] = row['fields']['Post Title']
-            match_row['assignee'] = row['fields']['Editor']['name']
-            try:
-                match_row['draft'] = row['fields']['Draft']
-            except KeyError:  # no blog draft link
-                match_row['draft'] = ""
-                print('Blog with id #{} has no Draft link.'.format(row['id']))
-            try:
-                match_row['issue'] = row['fields']['Issue']
-            except KeyError:  # No GitHub issue link
-                print('Blog with id #{} has no Issue link.'.format(row['id']))
-                match_row['issue'] = ""
+            for k, v in match_structure.items():
+                if len(v) == 2:
+                    add_match_field(row, match_row, v[0], v[1])
+                elif len(v) == 1:
+                    add_match_field(row, match_row, v[0])
+                else:
+                    print('ERROR: Match structure key {} has more values than '
+                          'it knows how to handle\n{}'.format(k, v))
             matches.append(match_row)
         return matches
-
-    # def add_match_field(self, table_row, match_row):
-    #     try:
-    #         match_row['issue'] = table_row['fields']['Issue']
-    #     except KeyError:  # No GitHub issue link
-    #         print('Blog with id #{} has no Issue link.'.format(table_row['id']))
-    #         match_row['issue'] = ""
 
     @property
     def filtered_table(self):
