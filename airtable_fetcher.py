5c9511d9c3ecdf76add2f370242557cd8798b871
dsjack <dsjack89@gmail.com>
2019-12-28 20:43:24 -0800

    Changes for loop in prep_matches() to a list comprehension

--- airtable_fetcher.py
+++ airtable_fetcher.py
@@ -1,32 +1,38 @@
-def add_match_field(table_row, match_row, field, sub_field=None):
+def add_match_field(table_row, structure_vals):
     """
     Catch any missing data in one row of an Airtable calendar
-    :param table_row: dict. an airtable calendar row
-    :param match_row: dict. an object to store match data
-    :param field: string.
-    :param sub_field: string (optional).
+    :param table_row: dict<String:String>. an airtable calendar row
+    :param fields: list<String>. A list of fields
     :return:
     """
-    f_lower = field.lower()
-    if sub_field == 'name':
-        f_lower = 'assignee'
-    elif 'title' in field.lower() or 'headline' in field.lower():
-        f_lower = 'title'
+    match_row = dict()
+    for fields in structure_vals:
+        field = fields[0]
+        sub_field = None
+        if len(fields) == 2:
+            sub_field = fields[1]
 
-    if sub_field:
-        try:
-            match_row[f_lower] = table_row['fields'][field][sub_field]
-        except KeyError:  # No value at field/sub-field for this match
-            print('Blog with id #{} has no value at: ["{}"]["{}"]...'.format(
-                table_row['id'], field, sub_field))
-            match_row[f_lower] = ""
-    else:
-        try:
-            match_row[f_lower] = table_row['fields'][field]
-        except KeyError:  # No value at field for this match
-            print('Blog with id #{} has no value at: ["{}"]...'.format(
-                table_row['id'], field))
-            match_row[f_lower] = ""
+        f_lower = field.lower()
+        if sub_field == 'name':
+            f_lower = 'assignee'
+        elif 'title' in field.lower() or 'headline' in field.lower():
+            f_lower = 'title'
+
+        if sub_field:
+            try:
+                match_row[f_lower] = table_row['fields'][field][sub_field]
+            except KeyError:  # No value at field/sub-field for this match
+                print('Blog with id #{} has no value at: ["{}"]["{}"]...'.format(
+                    table_row['id'], field, sub_field))
+                match_row[f_lower] = ""
+        else:
+            try:
+                match_row[f_lower] = table_row['fields'][field]
+            except KeyError:  # No value at field for this match
+                print('Blog with id #{} has no value at: ["{}"]...'.format(
+                    table_row['id'], field))
+                match_row[f_lower] = ""
+    return match_row
 
 
 class TableFetcher:
@@ -48,6 +54,7 @@ class TableFetcher:
         """
         print('GETTING matches with createdTime later than {}'.format(
             self._last_fetched))
+        local_filtered = []
         for row in self._airtable.get_all():
             if row['createdTime'] > self._last_fetched:
                 try:
@@ -61,6 +68,12 @@ class TableFetcher:
                         row['id']))
 
     def print_matches(self, match_structure, asana_preview=False):
+        """
+        For testing purposes only
+        :param match_structure:
+        :param asana_preview:
+        :return:
+        """
         table = self._filtered_table
         if asana_preview:
             table = self.prep_matches(match_structure)
@@ -71,19 +84,8 @@ class TableFetcher:
     def prep_matches(self, match_structure):
         if not self._filtered_table:
             return -1
-        matches = list()
-        for row in self._filtered_table:
-            match_row = dict()
-            for k, v in match_structure.items():
-                if len(v) == 2:
-                    add_match_field(row, match_row, v[0], v[1])
-                elif len(v) == 1:
-                    add_match_field(row, match_row, v[0])
-                else:
-                    print('ERROR: Match structure key {} has more values than '
-                          'it knows how to handle\n{}'.format(k, v))
-            matches.append(match_row)
-        return matches
+        return [add_match_field(row, match_structure.values()) for row in
+                self._filtered_table]
 
     @property
     def filtered_table(self):
